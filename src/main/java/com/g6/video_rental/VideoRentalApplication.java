325785d57d2479783a0fc0501aa0724974a897f7
MikaelFeher <mikael.feher@gmail.com>
2018-04-10 09:31:07 +0200

    Initial Commit

---
+++ src/main/java/com/g6/video_rental/VideoRentalApplication.java
@@ -0,0 +1,12 @@
+package com.g6.video_rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoRentalApplication.class, args);
+    }
+}
