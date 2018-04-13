37b408f8615f8e61b5c70a9b7425e260c9c9e0dd
MikaelFeher <mikael.feher@gmail.com>
2018-04-13 23:30:47 +0200

    WIP - Search functionality on the movies page

--- src/main/java/com/g6/video_rental/VideoRentalApplication.java
+++ src/main/java/com/g6/video_rental/VideoRentalApplication.java
@@ -21,27 +21,29 @@ public class VideoRentalApplication {
     @Bean
     CommandLineRunner runner(MovieRepository movieRepository, CustomerRepository customerRepository, RentedMovieRepository rentedMovieRepository) {
         return  args -> {
-            Movie movie = new Movie("Star Wars - A new hope", "Epic saga about the evil empire fighting the rebel alliance", "1977", "Science Fiction", "Blu-Ray", false);
-            Movie movie2 = new Movie("Star Wars - The empire strikes back", "Epic saga about the evil empire kicking the rebel alliance ass", "1980", "Science Fiction", "Blu-Ray", false);
-            Movie movie3 = new Movie("Star Wars - Return of the Jedi", "Epic saga about the evil empire getting it's ass kicked by the rebel alliance ", "1983", "Science Fiction", "Blu-Ray", false);
+
+            // Adding movies to the db...
+            for (int i = 0; i < 3; i++) {
+                movieRepository.save(new Movie("Star Wars - A new hope", "Epic saga about the evil empire fighting the rebel alliance", "1977", "Science Fiction", "Blu-Ray", false));
+                movieRepository.save(new Movie("Star Wars - The empire strikes back", "Epic saga about the evil empire kicking the rebel alliance ass", "1980", "Science Fiction", "Blu-Ray", false));
+                movieRepository.save(new Movie("Star Wars - Return of the Jedi", "Epic saga about the evil empire getting it's ass kicked by the rebel alliance ", "1983", "Science Fiction", "Blu-Ray", false));
+            }
+
+            // Adding customers to the db...
             Customer customer = new Customer("770325-1234", "Mikael", "Feher", "Tulesholmsgatan 97", "27437", "Skurup", "Sverige", "0704619961" ,"mikael.feher@gmail.com");
             Customer customer1 = new Customer("820916-1234", "Max", "Barnell", "Drottninggatan 0", "12345", "Helsingborg", "Sverige", "0987654321" ,"maxb@test.com");
             customerRepository.save(customer);
             customerRepository.save(customer1);
 
-            // Adding movies to the db AND marking them as rented...
-            movie.setRented(true);
-            movieRepository.save(movie);
-            movieRepository.save(movie2);
-            movieRepository.save(movie3);
 
-            // Customer renting a movie...
-            RentedMovie rm = new RentedMovie(customer, movie);
-            rentedMovieRepository.save(rm);
-            customer.getRentedMovies().add(rm);
 
-            System.out.println("Customer :" + customer.getRentedMovies());
-            System.out.println("Customer 1: " + customer1.getRentedMovies());
+//            // Customer renting a movie...
+//            RentedMovie rm = new RentedMovie(customer, movie);
+//            rentedMovieRepository.save(rm);
+//            customer.getRentedMovies().add(rm);
+//
+//            System.out.println("Customer :" + customer.getRentedMovies());
+//            System.out.println("Customer 1: " + customer1.getRentedMovies());
         };
     }
 }
--- src/main/java/com/g6/video_rental/controller/MovieController.java
+++ src/main/java/com/g6/video_rental/controller/MovieController.java
@@ -5,10 +5,9 @@ import com.g6.video_rental.domain.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -18,12 +17,54 @@ public class MovieController {
     @Autowired
     private MovieRepository movieRepository;
 
-//    Needs a redirect to the "/" getmapping when the url is 'localhost/movies'...
+//    Needs a redirect to the "/" getmapping when the url is 'http://localhost/movies'...
 
     @GetMapping("/")
     public String getMovies(Model model) {
         List<Movie> movies = (List<Movie>) movieRepository.findAll();
         model.addAttribute("movies", movies);
+        return "movies/movies";
+    }
+    @PostMapping("/")
+    public String findMovies(@ModelAttribute Movie movie, Model model) {
+        List<Movie> allMovies = (List<Movie>) movieRepository.findAll();
+        List<Movie> filteredMovies = new ArrayList<>();
+        List<Movie> tempMovies = new ArrayList<>();
+
+        for (Movie m : allMovies){
+            if (movie.getName() != null) {
+                tempMovies = movieRepository.findByName(movie.getName());
+                for (int i = 0; i < filteredMovies.size(); i++) {
+                    for (int j = 0; j < tempMovies.size(); j++) {
+                        if (tempMovies.get(j).getProductNumber() != filteredMovies.get(i).getProductNumber() ) {
+                            filteredMovies.add(tempMovies.get(j));
+                        }
+                    }
+                }
+            }
+//            if (movie.getCategory() != null) {
+//                tempMovies = movieRepository.findByCategory(category);
+//                for (int i = 0; i < filteredMovies.size(); i++) {
+//                    for (int j = 0; j < tempMovies.size(); j++) {
+//                        if (tempMovies.get(j).getProductNumber() != filteredMovies.get(i).getProductNumber() ) {
+//                            filteredMovies.add(tempMovies.get(j));
+//                        }
+//                    }
+//                }
+//            }
+//            if (releaseYear != null) {
+//                tempMovies = movieRepository.findByReleaseYear(releaseYear);
+//                for (int i = 0; i < filteredMovies.size(); i++) {
+//                    for (int j = 0; j < tempMovies.size(); j++) {
+//                        if (tempMovies.get(j).getProductNumber() != filteredMovies.get(i).getProductNumber() ) {
+//                            filteredMovies.add(tempMovies.get(j));
+//                        }
+//                    }
+//                }
+//            }
+        }
+
+        model.addAttribute("movies", filteredMovies);
         return "movies/movies";
     }
 
