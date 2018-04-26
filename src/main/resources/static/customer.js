280fe3cf59a6d5e82dbb8d1da576e84a59d3da82
MikaelFeher <mikael.feher@gmail.com>
2018-04-26 10:51:12 +0200

    Consolidates all javascript to main.js

--- src/main/resources/static/customer.js
+++
@@ -1,3 +0,0 @@
-function showCustomerDetails(socialSecurityNumber) {
-    window.location.assign(`/customer/customer?socialSecurityNumber=${socialSecurityNumber}`);
-}
--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,5 +1,6 @@
 console.log("Hello from main.js");
 
+/* Movies */
 function showMovieDetails(productNumber) {
     window.location.assign(`/movies/movie/${productNumber}/details`);
 }
@@ -12,9 +13,15 @@ function deleteMovie(productNumber) {
     window.location.assign(`/movies/movie/${productNumber}/delete`);
 }
 
+/* Rentals */
 function showRentalDetails(rentalId) {
     window.location.assign(`/rentedmovies/${rentalId}/rentaldetails`);
 }
 function showLateRentalDetails(rentalId) {
     window.location.assign(`/rentedmovies/latemovies/${rentalId}/laterentaldetails`);
+}
+
+/* Customers */
+function showCustomerDetails(socialSecurityNumber) {
+    window.location.assign(`/customer/customer?socialSecurityNumber=${socialSecurityNumber}`);
 }
