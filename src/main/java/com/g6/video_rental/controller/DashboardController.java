cce8cd09fbb50d5de19a14ea2c0a0fe78e0c36b7
Ervin Jusufagic <ervin_j_@hotmail.com>
2018-04-26 12:44:39 +0200

    dashboard done

--- src/main/java/com/g6/video_rental/controller/DashboardController.java
+++ src/main/java/com/g6/video_rental/controller/DashboardController.java
@@ -1,16 +1,33 @@
 package com.g6.video_rental.controller;
 
+
+import com.g6.video_rental.domain.Entities.RentedMovie;
+
+import com.g6.video_rental.domain.repository.RentedMovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/dashboard")
 public class DashboardController {
 
+
+    @Autowired
+    RentedMovieRepository rentedMovieRepository;
+
+
     @GetMapping("")
-    public String getDashboard(){
+    public String getDashboard(Model model){
+        List<RentedMovie> lateMovies = rentedMovieRepository.findByRentedDateLessThanAndReturnedDateNull(LocalDate.now().minusDays(1));
+        model.addAttribute("lateMovies", lateMovies);
+        model.addAttribute("title", "Försenade filmer");
+
         return "dashboard/dashboard";
     }
 }
